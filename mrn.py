7c338d7c02f708f16e3e06df882d1d42e7888a2a
Nick <nickkossolapov@gmail.com>
2017-06-27 11:10:56 +0200

    Miscellaneous Fixed

--- mrn.py
+++ mrn.py
@@ -6,9 +6,10 @@ from data_processor import get_smooth_data
 from post import get_data
 from simulate import run_simulation
 from scipy.optimize import minimize
-from optimiser import get_plasticity, get_sum_squares, test_plasticity
+from optimiser import get_plasticity, get_sum_squares
 
-#K: 892.836650801792,    n: 0.40923596690313946
+# K: 870.81973,	n: 0.41040
+# K: 878.72981, n: 0.44389
 
 eval_counter = 0
 ccx_params = {"mid_time": 0.6, "end_disp": 0.9, "amplitude": -1.547}
@@ -17,7 +18,7 @@ def main():
     log = create_log()
     log.info("--- Program started at %s ---", datetime.datetime.now().strftime("%H:%M:%S"))
 
-    solution = minimize(optimise_function, (45000, 1.15), method='Nelder-Mead', tol = 1, args=(log))
+    solution = minimize(eval_function, (878, 0.42), method='Nelder-Mead', tol = 1e-6, args=(log))
     log.info(solution)
 
     disp, force = get_data(eval_counter-1, ccx_params)
@@ -25,22 +26,23 @@ def main():
     plt.plot(h_exp, f_exp)
     plt.plot(disp, force)
     plt.show()
+    # log.info("\n--- Program completed at %s ---\n", datetime.datetime.now().strftime("%H:%M:%S"))
 
     for handler in log.handlers:
         handler.close()
         log.removeFilter(handler)
 
 
-def optimise_function(fh, log):
+def eval_function(cnst, log):
     global eval_counter
 
     file_num = eval_counter
-    stresses, strains = get_plasticity(fh[0], fh[1])
+    stresses, strains = get_plasticity(cnst[0], cnst[1])
     run_simulation(file_num, stresses, strains, ccx_params)
     disp, force = get_data(file_num, ccx_params)
 
-    ssum = get_sum_squares(disp, force, 50, 1e-1)
-    log.info("K: %.4f,\tn: %.4f,\tSum: %.5f", fh[0], fh[1], ssum)
+    ssum = get_sum_squares(disp, force, 50, 1)
+    log.info("K: %.5f,\tn: %.5f,\tSum: %.5f", cnst[0], cnst[1], ssum)
 
     eval_counter += 1
     return ssum
