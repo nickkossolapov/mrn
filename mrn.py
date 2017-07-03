ae9a84e88ae915613f066a7a0d2af3d2e9422314
Nick <nickkossolapov@gmail.com>
2017-07-03 16:49:14 +0200

    Added n parameter logging

--- mrn.py
+++ mrn.py
@@ -8,24 +8,19 @@ from simulate import run_simulation
 from scipy.optimize import minimize
 from optimiser import get_plasticity, get_sum_squares
 
-# K: 870.81973,	n: 0.41040
-# K: 878.72981, n: 0.44389
+# K: 297.45,  n: 0.519,
 
 eval_counter = 0
-ccx_params = {"mid_time": 0.6, "end_disp": 0.9, "amplitude": -1.547}
+ccx_params = {"mid_time": 0.6, "end_disp": 0.9, "amplitude": -1.547, "spring_constant": 2.1e6}
 
 def main():
     log = create_log()
     log.info("--- Program started at %s ---", datetime.datetime.now().strftime("%H:%M:%S"))
 
-    solution = minimize(eval_function, (500, 0.4), method='Nelder-Mead', tol = 1e-6, args=(log))
-    log.info(solution)
+    simplex = [[245, 900, 2500, 12], [250, 950, 1750, 12.5], [255, 1000, 2000, 13], [260, 1050, 2250, 13.5], [265, 1100, 2500, 14]]
 
-    disp, force = get_data(eval_counter-1, ccx_params)
-    h_exp, f_exp = get_smooth_data()
-    plt.plot(h_exp, f_exp)
-    plt.plot(disp, force)
-    plt.show()
+    solution = minimize(eval_function, [255, 1000, 2000, 13], args = (log))
+    log.info(solution)
     log.info("\n--- Program completed at %s ---\n", datetime.datetime.now().strftime("%H:%M:%S"))
 
     for handler in log.handlers:
@@ -37,12 +32,18 @@ def eval_function(cnst, log):
     global eval_counter
 
     file_num = eval_counter
-    stresses, strains = get_plasticity(cnst[0], cnst[1])
+    stresses, strains = get_plasticity(cnst, 50, "log")
     run_simulation(file_num, stresses, strains, ccx_params)
     disp, force = get_data(file_num, ccx_params)
 
     ssum = get_sum_squares(disp, force, 50, 1)
-    log.info("K: %.5f,\tn: %.5f,\tSum: %.5f", cnst[0], cnst[1], ssum)
+
+    logstring = ""
+    for i in range(len(cnst)):
+        logstring += "\t{:c}: {:.5f},".format(97+i, cnst[i])
+
+    logstring += "\tSum: {:.5f}".format(ssum)
+    log.info(logstring)
 
     eval_counter += 1
     return ssum
