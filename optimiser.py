63f87349ae1478426c5c69ed335e4f5b0d9acfce
Nick <nickkossolapov@gmail.com>
2017-07-18 15:29:29 +0200

    Added edge adjustments to MSE function

--- optimiser.py
+++ optimiser.py
@@ -46,8 +46,10 @@ def get_plasticity(par, N, model, spacing = "log"):
 
     return list(stresses), list(strains)
 
-def get_sum_squares(h, f, N, weighting = 1, curve = "full", scale = 1):
-    """Usage: list h, list f, int N, float weighting, float scale
+def get_sum_squares(h, f, N, curve = "full", scale = 1, limits = (0.05, 0.05, 0.05, 0.05) , weighting = 1):
+    """Usage: list h, list f, int N, float weighting, float scale, tuple limits
+
+    limits is [loading upper, loading lower, unloading upper, unloading lower], where bounds are definied by min and max of data set.
 
     curve is either "full", "loading", or "unloading"
 
@@ -56,9 +58,8 @@ def get_sum_squares(h, f, N, weighting = 1, curve = "full", scale = 1):
     fh_exp = _split_data(h_exp, f_exp)
     fh_fem = _split_data(h, f)
 
-
-    ssum1 = _get_piecewise_ss(fh_exp[0], fh_exp[1], fh_fem[0], fh_fem[1], N//2+N%2)
-    ssum2 = weighting*_get_piecewise_ss(fh_exp[2], fh_exp[3], fh_fem[2], fh_fem[3], N//2)
+    ssum1 = _get_piecewise_ss(fh_exp[0], fh_exp[1], fh_fem[0], fh_fem[1], N//2+N%2, limits[0], limits[1])
+    ssum2 = weighting*_get_piecewise_ss(fh_exp[2], fh_exp[3], fh_fem[2], fh_fem[3], N//2, limits[2], limits[3])
     log.info("Sum of squares: %.4f,\t %.4f", ssum1, ssum2)
 
     if curve == "full":
@@ -70,16 +71,16 @@ def get_sum_squares(h, f, N, weighting = 1, curve = "full", scale = 1):
 
     return ssum
 
-def _get_piecewise_ss(h_exp, f_exp, h_fem, f_fem, N):
+def _get_piecewise_ss(h_exp, f_exp, h_fem, f_fem, N, up_limit, lo_limit):
     p_ssum = 0
     if h_exp[0] > h_exp[-1]:
-        for h in np.linspace(min(h_exp)+0.05, max(h_exp)-0.05, N):
+        for h in np.linspace(min(h_exp)+lo_limit, max(h_exp)-up_limit, N):
             f1 = np.interp(h, h_exp[::-1], f_exp[::-1])
             f2 = np.interp(h, h_fem[::-1], f_fem[::-1])
             p_ssum += ((f2-f1)/1000)**2
 
     else:
-        for h in np.linspace(min(h_exp)+0.05, max(h_exp)-0.05, N):
+        for h in np.linspace(min(h_exp)+lo_limit, max(h_exp)-up_limit, N):
             f1 = np.interp(h, h_exp, f_exp)
             f2 = np.interp(h, h_fem, f_fem)
             p_ssum += ((f2-f1)/1000)**2
